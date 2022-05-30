6eb6275f94b6bd846618239f74584426aeea41b5
KaiGeller <gellerka@oregonstate.edu>
2022-05-29 21:44:16 -0700

    1

--- StreamingGuide.py
+++ StreamingGuide.py
@@ -1,61 +1,61 @@
 class Movie:
+
    def __init__ (self, title, genre, director, year):
        self.title = title
        self.genre = genre
        self.director = director
        self.year = year
+
    def get_title(self):
        return self.title
+
    def get_genre(self):
        return self.genre
+
    def get_director(self):
        return self.director
+
    def get_year(self):
        return self.year
 
 class StreamingService:
+
    def __init__ (self, name):
        self.name = name
        self.catalog = {}
+
    def get_name(self):
        return self.name
+
    def get_catalog(self):
        return self.catalog
+
    def add_movie(self, movie):
-       movie_details = [movie.get_genre(), movie.get_director(), movie.get_year()]
-       self.catalog[movie.get_title()] = movie_details
+       self.catalog[movie.get_title()] = movie
+
    def delete_movie(self, movie):
-       if movie in self.catalog is False:
+       if movie in self.catalog is True:
            del self.catalog[movie.get_title()]
+
 class StreamingGuide:
+
    def __init__ (self):
        self.streaming_service_list = []
+
    def add_streaming_service (self, streaming_service):
        self.streaming_service_list.append(streaming_service)
+
    def delete_streaming_service (self, streaming_service):
        for i in self.streaming_service_list:
            if i == streaming_service:
                self.streaming_service_list.remove(i)
+
    def where_to_watch(self, movie):
        search_results = []
-       search_results.append(movie.get_title())
        for i in self.streaming_service_list:
-           print("check1")
-
-           print((movie.get_title()) in i.get_catalog())
-           if movie.get_title() in i.get_catalog() is True:
-
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
+           if movie in i.get_catalog():
                search_results.append(i.get_name())
-               print("check2")
-       print(search_results)
-       print(len(search_results))
-       if len(search_results) < 2:
-           search_results.clear()
-           search_results.append("none")
        return search_results
-
 
 
 
